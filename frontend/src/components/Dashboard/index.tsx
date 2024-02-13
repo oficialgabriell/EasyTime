4c0a2b23ddad324d33da8e3d4b8545896fb336b6
Igorss69 <igor.butowisk@gmail.com>
2024-02-13 02:16:46 -0300

    buttongroup feito

--- frontend/src/components/Dashboard/index.tsx
+++ frontend/src/components/Dashboard/index.tsx
@@ -1,11 +1,24 @@
-function Dashboard(){
-    return(
-        <>
-            <div>
-                <h1>ola mundo</h1>
-            </div>
-        </>
-    )
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import './style.css'; // Importe o arquivo CSS para este componente
+
+
+function VerticalExample() {
+  return (
+    <div className='vertical-container w-25' id='div-options'>
+      <div className="button-group-container d-flex h-100">
+        <ButtonGroup vertical className="full-height w-100">
+          <Button><h3>Dashboard</h3></Button>
+          <Button><h3>Funcionários</h3></Button>
+          <Button><h3>Cadastrar</h3></Button>
+          <Button><h3>Horarios</h3></Button>
+          <Button><h3>Impressões</h3></Button>
+        </ButtonGroup>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard;
+export default VerticalExample;
