7a2de1069009be55f2a949e4e501f0239869530e
Igorss69 <igor.butowisk@gmail.com>
2024-02-12 21:07:10 -0300

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainApp from './components/Main';
+import PainelLogin from './components/Painel';
 
 function App(): JSX.Element {
   return (
     <React.Fragment>
       <Router>
         <Routes>
-          <Route element={<MainApp />} path="/" />
+          <Route element={<PainelLogin />} path="/" />
         </Routes>
       </Router>
     </React.Fragment>
