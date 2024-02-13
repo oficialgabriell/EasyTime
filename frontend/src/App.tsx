0fa12b066cbd41823b0f865e453e77e08c986bf1
Igorss69 <igor.butowisk@gmail.com>
2024-02-13 00:53:58 -0300

    first commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainApp from './components/Main';
 import PainelLogin from './components/Painel';
+import Dashboard from './components/Dashboard';
 
 function App(): JSX.Element {
   return (
@@ -11,6 +12,7 @@ function App(): JSX.Element {
       <Router>
         <Routes>
           <Route element={<PainelLogin />} path="/login" />
+          <Route element={<Dashboard />} path="/dashboard" />
         </Routes>
       </Router>
     </React.Fragment>
