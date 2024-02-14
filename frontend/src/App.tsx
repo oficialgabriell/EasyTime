d3b58fa588d0cc821e85076cc63bbeb354b647cc
Gabriel <gabrieloficiall57@gmail.com>
2024-02-13 22:44:40 -0300

    Estilização do painel de login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PainelLogin from './components/Painel';
-import CadFuncionario from './components/CadFuncionario';
-import ListFuncionarios from './components/ListFuncionarios';
 
 function App(): JSX.Element {
   return (
     <React.Fragment>
       <Router>
         <Routes>
-          {/* <Route element={<PainelLogin />} path="/" /> */}
-          {/* <Route element={<CadFuncionario />} path='/' /> */}
-          <Route element={<PainelLogin />} path='/' />
+          <Route element={<PainelLogin />} path='/login' />
         </Routes>
       </Router>
     </React.Fragment>
--- frontend/src/components/Painel/index.tsx
+++ frontend/src/components/Painel/index.tsx
@@ -1,40 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Form, Container, Card } from 'react-bootstrap';
 import './style.scss';
 const PainelLogin: React.FC = () => {
   const [matricula, setMatricula] = useState('');
   const [password, setPassword] = useState('');
 
+  useEffect(() => {
+    // Função para bloquear o zoom com o scroll do mouse
+    const handleWheel = (e: WheelEvent) => {
+      if (e.ctrlKey || e.metaKey) {
+        e.preventDefault();
+      }
+    };
+
+    // Adiciona o listener do evento de rolagem do mouse
+    window.addEventListener('wheel', handleWheel, { passive: false });
+
+    // Remove o listener quando o componente for desmontado
+    return () => {
+      window.removeEventListener('wheel', handleWheel);
+    };
+  }, []); // O array vazio [] assegura que o useEffect será executado apenas uma vez durante a montagem do componente
+
+
   return (
-    <div className=' bg-Custom' style={{paddingTop: '8.75rem'}}>
-    <Container className="mt-5 d-flex align-items-center">
+    <div className='bg-Custom' style={{ paddingTop: '2rem', display: 'flex'}}>
+    <Container className="mt-5 d-flex align-items-center" style={{ width: '70%', height: '70%' }}>
       <Card className='bg-transparent'>
-        <Card.Header className='text-center customColor grayBackground'>
+        <Card.Header className='text-center customColor grayBackground rounded'>
           <h2>Login</h2>
         </Card.Header>
         <Card.Body>
           <Form>
             <Form.Group className="" controlId="formBasicMatricula">
-              <Form.Label>Matrícula</Form.Label>
+              <Form.Label></Form.Label>
               <Form.Control
                 type="text"
-                placeholder="@123456"
+                placeholder="Matrícula"
                 value={matricula}
                 onChange={(e) => setMatricula(e.target.value)}
               />
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
-              <Form.Label>Senha</Form.Label>
+              <Form.Label></Form.Label>
               <Form.Control
                 type="password"
-                placeholder="Digite sua senha"
+                placeholder="Senha"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
             </Form.Group>
 
-            <Button className=""variant="primary" type="button">
+            <Button className="d-block mx-auto mt-3"variant="primary" type="button">
               Entrar
             </Button>
           </Form>
