61cdd6febeb75d55be9ebb79ba3792334b56f6b2
Gabriel <gabrieloficiall57@gmail.com>
2024-02-17 23:31:49 -0300

    Logo do painel adicionada

--- frontend/src/components/Painel/index.tsx
+++ frontend/src/components/Painel/index.tsx
@@ -11,7 +11,7 @@ const PainelLogin: React.FC = () => {
         e.preventDefault();
       }
     };
-    
+
     window.addEventListener('wheel', handleWheel, { passive: false });
 
     return () => {
@@ -21,42 +21,54 @@ const PainelLogin: React.FC = () => {
 
 
   return (
-    <div className='bg-Custom' style={{ paddingTop: '2rem', display: 'flex'}}>
-    <Container className="mt-5 d-flex align-items-center w-100" style={{ width: '60rem'}}>
-      <Card className='bg-transparent border-0 mx-2 w-20rem'>
-        <Card.Header className='text-center customColor grayBackground rounded'>
-          <h2>Login</h2>
-        </Card.Header>
-        <Card.Body>
-          <Form>
-            <Form.Group className="" controlId="formBasicMatricula">
-              <Form.Label></Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Matrícula"
-                value={matricula}
-                onChange={(e) => setMatricula(e.target.value)}
-                className='my-2'
-              />
-            </Form.Group>
+    <div className='bg-Custom'>
+      <Container className="mt-5 d-flex align-items-center w-100" style={{ width: '60rem' }}>
+        <img
+          src="https://i.imgur.com/TrLvVCq.jpg"  // Substitua pela URL direta da imagem
+          alt="Logo"
+          width="600"
+          height="600"
+          style={{
+            float: 'right',
+            marginRight: '0',
+            marginLeft: 'auto', // Aspas simples corretamente fechadas aqui
+            marginTop: '-3.75rem'
+          }}
+        />
+        <Card className='bg-transparent border-0 mx-2 w-20rem'>
+          <Card.Header className='text-center customColor grayBackground rounded'>
+            <h2>Login</h2>
+          </Card.Header>
+          <Card.Body>
+            <Form>
+              <Form.Group className="" controlId="formBasicMatricula">
+                <Form.Label></Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Matrícula"
+                  value={matricula}
+                  onChange={(e) => setMatricula(e.target.value)}
+                  className='my-2'
+                />
+              </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label></Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Senha"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Form.Group>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label></Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Senha"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
 
-            <Button className="d-block mx-auto mt-5"variant="primary" type="button">
-              Entrar
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-    </Container>
+              <Button className="d-block mx-auto mt-5" variant="primary" type="button">
+                Entrar
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
 
   );
